5ff403ac55c14d8c1dbccd24748ad7ee26201762
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-03-04 14:30:33 +0200

    pre-rendering and static data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData, PostData } from "../lib/posts";
 
-export default function Home() {
+interface PropType {
+  allPostsData: PostData[];
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home(props: PropType) {
+  const { allPostsData } = props;
+
   return (
     <Layout home={true}>
       <Head>
@@ -10,6 +26,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello! I'm Mingyum Kim from South Korea.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
